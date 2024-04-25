3ad73c87a666be3e17a4b49b376e0fe2c291eb91
Degilengenharia <147849397+Degilengenharia@users.noreply.github.com>
2024-04-25 20:21:37 -0300

    Update config.local.ts
    alterado valor base de m² de 60 para 40

--- config.local.ts
+++ config.local.ts
@@ -3,7 +3,7 @@ export const config = {
     PHONE: "51992646568", // celular whatsapp
     ALWAYS_SEND_EMAIL: true, // Envia emails sempre, mesmo sem email informado
 
-    INITIAL_M2: 60, // m² inicial
+    INITIAL_M2: 40, // m² inicial
     INITIAL_PRICE: 100, // Preço inicial
     SURPLUS_M2: 1, // Valor do m² excedente
     SEMI_FURNISHED: 0.20, // Valor adicional(%) Semi Mobiliado
